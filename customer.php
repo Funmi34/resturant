29e011674caa26f013da72eca5f98a6df52f800a
Funmi34 <dabolaura@gmail.com>
2023-08-29 05:01:48 -0700

    first commit in resturant

--- customer.php
+++ customer.php
@@ -7,6 +7,7 @@
 </head>
 <body>
     <h1>Welcome to customers page</h1>
+<p>document</p>
     <ul>
         <li><a href="attendant.php">Attendant page</a></li>
         <li><a href="customer.php">Customer page</a></li>
